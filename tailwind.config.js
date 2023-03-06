675fceb3fa85a69c4006b1c25e65b259d89d9c7a
joagaloppo <joaquingaloppo@gmail.com>
2023-03-06 09:48:40 -0300

    feat: Add Tailwind and configure files

---
+++ tailwind.config.js
@@ -0,0 +1,15 @@
+const defaultTheme = require('tailwindcss/defaultTheme');
+
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+	content: ['./src/**/*.{js,ts,jsx,tsx}'],
+	theme: {
+		extend: {
+			fontFamily: {
+				sans: ['Inter', ...defaultTheme.fontFamily.sans],
+				display: ['Lexend', ...defaultTheme.fontFamily.sans],
+			},
+		},
+	},
+	plugins: [],
+};
