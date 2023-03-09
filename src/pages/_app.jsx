39e15eee7e4bf23e84a9b52ae503ecab4b595348
joagaloppo <joaquingaloppo@gmail.com>
2023-03-09 15:46:27 -0300

    feat: Add slim progress bar for page loading

--- src/pages/_app.jsx
+++ src/pages/_app.jsx
@@ -1,5 +1,20 @@
 import '@/styles/tailwind.css';
 
+import NProgress from 'nprogress';
+import 'nprogress/nprogress.css';
+import Router from 'next/router';
+
+NProgress.configure({
+	minimum: 0.5,
+	easing: 'ease',
+	speed: 300,
+	showSpinner: false,
+});
+
+Router.events.on('routeChangeStart', () => NProgress.start());
+Router.events.on('routeChangeComplete', () => NProgress.done());
+Router.events.on('routeChangeError', () => NProgress.done());
+
 export default function App({ Component, pageProps }) {
 	return <Component {...pageProps} />;
 }
